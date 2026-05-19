14d10e6fd3af3af96f46ec18ceb4c96f087f97e7
supesu <supesu.dev@gmail.com>
2026-05-19 23:51:30 +1000

    feat(fm-agent): setup grpc stubs + grpc connection

--- data-plane/cmd/fm-agent/main.go
+++ data-plane/cmd/fm-agent/main.go
@@ -1,22 +1,69 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/supesu/faultmesh/data-plane/pkg/logging"
 	"github.com/supesu/faultmesh/data-plane/pkg/signals"
 	"github.com/supesu/faultmesh/data-plane/pkg/version"
+
+	pb "github.com/supesu/faultmesh/data-plane/pkg/genproto/faultmesh/v1"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	logger := logging.New()
 
 	logger.Info().
-		Str("Verison", version.Version).
-		Str("Git Commit", version.GitCommit).
+		Str("Version", version.Version).
+		Str("GitCommit", version.GitCommit).
 		Str("Built", version.BuildDate).
-		Msg("fm-agent")
+		Msg("fm-agent starting")
 
-	// pause until shutdown.
-	ctx, stop := signals.Context()
+	controlAddr := flag.String("control-addr", "", "control plane address")
+	flag.Parse()
+
+	if *controlAddr == "" {
+		logger.Error().Msg("--control-addr is required")
+		os.Exit(1)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(
+		ctx,
+		*controlAddr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+
+	if err != nil {
+		logger.Error().Err(err).
+			Str("addr", *controlAddr).
+			Msg("failed to connect to control plane")
+		os.Exit(1)
+	}
+
+	client := pb.NewControlServiceClient(conn)
+
+	_ = client // stub that jawn for now
+
+	logger.Info().
+		Str("addr", *controlAddr).
+		Msg("connected to control plane (stub)")
+
+	// wait for shutdown
+	ctx2, stop := signals.Context()
 	defer stop()
-	<-ctx.Done()
+
+	<-ctx2.Done()
+
+	fmt.Println("shutdown complete")
 }
--- data-plane/go.mod
+++ data-plane/go.mod
@@ -6,5 +6,10 @@ require (
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/rs/zerolog v1.35.1 // indirect
-	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/sys v0.42.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260226221140-a57be14db171 // indirect
+	google.golang.org/grpc v1.81.1 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 )
